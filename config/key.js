d8b27dfe855c637d9a90aeba29cf9e7da13e02c9
aio <tom.shegzy@gmail.com>
2019-09-13 21:33:13 +0100

    Working On Mongooos

--- config/key.js
+++ config/key.js
@@ -1,8 +1,9 @@
 module.exports={
     
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "shopping_list"
+        host: "sql306.epizy.com",
+        user: "epiz_24330482",
+        password: "yyUETttP8xTD",
+        database: "epiz_24330482_shopping_list",
+        mongoURI:"mongodb+srv://aio:<segun12345>@cluster0-1hpps.mongodb.net/test?retryWrites=true&w=majority"
      
 }
